b09160506c24b3f16d6a3122ff9da78feb1445e2
Peter Horvath <horvthpeter@gmail.com>
2014-08-22 21:12:36 +0000

    Added some template inheritance. Added some styles.

--- index.py
+++ index.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/clrs')
+def clrs():
+    return render_template('clrs.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
