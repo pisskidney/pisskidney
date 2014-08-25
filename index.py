e62808ad775c3bc76bad3b495b7bbefb43eb73e1
Peter Horvath <horvthpeter@gmail.com>
2014-08-25 13:10:25 +0000

    Implemented the clrs solutions page.

--- index.py
+++ index.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+import os
+import re
+from flask import Flask, render_template, session
+
+
 app = Flask(__name__)
+
+
+CLRS_DIR = 'clrs_exercises'
+CLRS_CHAPTER_PREFIX = 'chapter_'
 
 
 @app.route('/')
@@ -9,8 +17,26 @@ def index():
 
 @app.route('/clrs')
 def clrs():
-    return render_template('clrs.html')
+    clrs_dir = os.path.join(app.template_folder, CLRS_DIR)
+    problems = dict()
+    for chapter in os.listdir(clrs_dir):
+        _, chapter_id = chapter.split('_')
+        for filename in os.listdir(os.path.join(clrs_dir, chapter)):
+            m = re.match('([0-9]+)_([0-9]+).html', filename)
+            section, problem = m.groups()
+            chapter_dict = problems.setdefault(chapter_id, {})
+            chapter_dict.setdefault(section, []).append(problem)
+    return render_template('clrs.html', problems=problems)
+
+
+@app.route('/clrs/<c>.<s>-<p>')
+def clrs_ex(c, s, p):
+    chapter = CLRS_CHAPTER_PREFIX + c
+    template_dir = os.path.join(CLRS_DIR, chapter)
+    template_path = os.path.join(template_dir, s + '_' + p + '.html')
+    return render_template(template_path)
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run('0.0.0.0')
